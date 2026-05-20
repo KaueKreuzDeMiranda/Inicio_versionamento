a7427707de2b0c0700c7aeb307b6b9f4a3395f5e
KaueKreuzDeMiranda <kauekmirannda@gmail.com>
2026-05-20 19:29:46 -0300

    Adição de novas funções

--- calculadora.py
+++ calculadora.py
@@ -12,6 +12,12 @@ def divisao(a, b):
         return "Não é possível dividir por 0!"
     else:
         return a / b
+    
+def exponenciacao(a, b):
+    return a**b
+
+def porcentagem(a, b):
+    return a * (b / 100)
 
 while True:
     print("\n===== Calculadora =====")
@@ -19,12 +25,14 @@ while True:
     print("\n2 - Subtração")
     print("\n3 - Multiplicação")
     print("\n4 - Divisão")
+    print("\n5 - Exponenciação")
+    print("\n6 - Porcntagem total/porcentagem")
     print("\n0 - Sair")
     
-    num1 = float(input("Digite o primeiro número: "))
-    num2 = float(input("Digite o segundo número: "))
-    
     opcao = int(input("\nEscolha uma opção: "))
+    
+    num1 = float(input("\nDigite o primeiro número: "))
+    num2 = float(input("\nDigite o segundo número: "))
     
     if opcao == 1:
         resultado = soma(num1, num2)
@@ -37,6 +45,12 @@ while True:
         print(f"\nResultado: {resultado}")
     elif opcao == 4:
         resultado = divisao(num1, num2)
+        print(f"\nResultado: {resultado}")
+    elif opcao == 5:
+        resultado = exponenciacao(num1, num2)
+        print(f"\nResultado: {resultado}")
+    elif opcao == 6:
+        resultado = porcentagem(num1, num2)
         print(f"\nResultado: {resultado}")
     elif opcao == 0:
         break
